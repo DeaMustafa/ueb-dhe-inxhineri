a5c5c52ffd4a0649e39dcf0b13b14cf7b7ad3272
lediona` <lediona.zubaku@.gmail.com`>
2023-02-24 13:03:06 +0100

    php

--- assets/js/login.js
+++
@@ -1,9 +0,0 @@
-const signup = document.querySelector('.t-signup');
-const login = document.querySelector('.t-login');
-const addClass = document.querySelector('.site')
-signup.addEventListener('click', function() {
-    addClass.className='site signup-show';
-})
-login.addEventListener('click', function() {
-    addClass.className = 'site login-show';
-})
